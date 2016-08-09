fe20a0ffc6e86772a4d2cfebf2754f3f09679c3a
Nyan Of The Moon <nyanofthemoon@gmail.com>
2016-08-09 17:51:01 -0400

    test

--- src/modules/api.js
+++ src/modules/api.js
@@ -184,37 +184,37 @@ class Api {
     room.setStatus(this.config.room.STATUS_AUDIO)
     room.setVideo(false)
     this.sockets.to(name).emit('query', room.query())
-    setTimeout(function() { let room = that.getRoomByName(name); if (room) {
+    setTimeout(function() {
       that.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_AUDIO_SELECTION)
       room.setStatus(that.config.room.STATUS_AUDIO_SELECTION)
       that.sockets.to(name).emit('query', room.query())
       // STATUS_AUDIO_SELECTION
-      setTimeout(function() { let room = that.getRoomByName(name); if (room) {
+      setTimeout(function() {
         that.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_AUDIO_RESULTS)
         room.setStatus(that.config.room.STATUS_AUDIO_RESULTS)
         that.sockets.to(name).emit('query', room.query())
         // STATUS_AUDIO_RESULTS
-        setTimeout(function() { let room = that.getRoomByName(name); if (room) {
+        setTimeout(function() {
           that.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_VIDEO)
           room.setStatus(that.config.room.STATUS_VIDEO)
           room.setVideo(true)
           that.sockets.to(name).emit('query', room.query())
           // STATUS_VIDEO
-          setTimeout(function() { let room = that.getRoomByName(name); if (room) {
+          setTimeout(function() {
             that.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_VIDEO_SELECTION)
             room.setStatus(that.config.room.STATUS_VIDEO_SELECTION)
             that.sockets.to(name).emit('query', room.query())
             // STATUS_VIDEO_SELECTION
-            setTimeout(function() { let room = that.getRoomByName(name); if (room) {
+            setTimeout(function() {
               // STATUS_VIDEO_RESULTS
               that.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_VIDEO_RESULTS)
               room.setStatus(that.config.room.STATUS_VIDEO_RESULTS)
               that.sockets.to(name).emit('query', room.query())
-            }}, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-          }}, (that.config.room.WAIT_TIME_VIDEO_CONVERSATION + that.config.room.NETWORK_RESPONSE_DELAY))
-        }}, (that.config.room.WAIT_TIME_RESULT_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-      }}, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-    }}, (this.config.room.WAIT_TIME_AUDIO_CONVERSATION + this.config.room.NETWORK_RESPONSE_DELAY))
+            }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
+          }, (that.config.room.WAIT_TIME_VIDEO_CONVERSATION + that.config.room.NETWORK_RESPONSE_DELAY))
+        }, (that.config.room.WAIT_TIME_RESULT_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
+      }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
+    }, (this.config.room.WAIT_TIME_AUDIO_CONVERSATION + this.config.room.NETWORK_RESPONSE_DELAY))
   }
 
   bindSocketToPublicEvents(socket) {
@@ -397,6 +397,7 @@ class Api {
         let room = this.getRoomByName(socket.room)
         if (room) {
           this.removeRoomFromQueue(room)
+          this.removeRoomFromAssoc(room)
         }
         this.sockets.to(roomName).emit('leave', socket.id)
         socket.leave(roomName)
