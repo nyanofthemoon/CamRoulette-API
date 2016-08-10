8435f6b3c68596d170b5b585f5aa2175dfb3567a
Nyan Of The Moon <nyanofthemoon@gmail.com>
2016-08-10 08:29:44 -0400

    fixes

--- src/modules/api.js
+++ src/modules/api.js
@@ -177,59 +177,77 @@ class Api {
   }
 
   runTimer(room) {
-    let that = this
-    let name = room.getName()
-    // STATUS_AUDIO
-    this.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_AUDIO)
-    room.setStatus(this.config.room.STATUS_AUDIO)
-    room.setVideo(false)
-    this.sockets.to(name).emit('query', room.query())
-    setTimeout(function() { if (room.getSocketIds().length > 1) {
-      that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_AUDIO_SELECTION)
-      room.setStatus(that.config.room.STATUS_AUDIO_SELECTION)
-      that.sockets.to(name).emit('query', room.query())
-      // STATUS_AUDIO_SELECTION
-      setTimeout(function() { if (room.getSocketIds().length > 1) {
-        that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_AUDIO_RESULTS)
-        room.setStatus(that.config.room.STATUS_AUDIO_RESULTS)
-        that.sockets.to(name).emit('query', room.query())
-        // STATUS_AUDIO_RESULTS
-        setTimeout(function() { if (room.getSocketIds().length > 1) {
-          that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO)
-          room.setStatus(that.config.room.STATUS_VIDEO)
-          room.setVideo(true)
+    try {
+      let that = this
+      let name = room.getName()
+      // STATUS_AUDIO
+      this.logger.verbose('[TIMER] ' + name + ' ' + this.config.room.STATUS_AUDIO)
+      room.setStatus(this.config.room.STATUS_AUDIO)
+      room.setVideo(false)
+      this.sockets.to(name).emit('query', room.query())
+      setTimeout(function () {
+        if (room.getSocketIds().length > 1) {
+          that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_AUDIO_SELECTION)
+          room.setStatus(that.config.room.STATUS_AUDIO_SELECTION)
           that.sockets.to(name).emit('query', room.query())
-          // STATUS_VIDEO
-          setTimeout(function() { if (room.getSocketIds().length > 1) {
-            that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO_SELECTION)
-            room.setStatus(that.config.room.STATUS_VIDEO_SELECTION)
-            that.sockets.to(name).emit('query', room.query())
-            // STATUS_VIDEO_SELECTION
-            setTimeout(function() { if (room.getSocketIds().length > 1) {
-              // STATUS_VIDEO_RESULTS
-              that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO_RESULTS)
-              room.setStatus(that.config.room.STATUS_VIDEO_RESULTS)
+          let sockets = room.getSockets()
+          sockets.forEach(function (socket) {
+            socket.leave(name)
+          })
+          // STATUS_AUDIO_SELECTION
+          setTimeout(function () {
+            if (room.getSocketIds().length > 1) {
+              that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_AUDIO_RESULTS)
+              room.setStatus(that.config.room.STATUS_AUDIO_RESULTS)
               that.sockets.to(name).emit('query', room.query())
+              // STATUS_AUDIO_RESULTS
+              setTimeout(function () {
+                if (room.getSocketIds().length > 1) {
+                  that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO)
+                  room.setStatus(that.config.room.STATUS_VIDEO)
+                  room.setVideo(true)
+                  that.sockets.to(name).emit('query', room.query())
+                  // STATUS_VIDEO
+                  setTimeout(function () {
+                    if (room.getSocketIds().length > 1) {
+                      that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO_SELECTION)
+                      room.setStatus(that.config.room.STATUS_VIDEO_SELECTION)
+                      that.sockets.to(name).emit('query', room.query())
+                      // STATUS_VIDEO_SELECTION
+                      setTimeout(function () {
+                        if (room.getSocketIds().length > 1) {
+                          // STATUS_VIDEO_RESULTS
+                          that.logger.verbose('[TIMER] ' + name + ' ' + that.config.room.STATUS_VIDEO_RESULTS)
+                          room.setStatus(that.config.room.STATUS_VIDEO_RESULTS)
+                          that.sockets.to(name).emit('query', room.query())
+                        } else {
+                          room.setStatus(that.config.room.STATUS_TERMINATED)
+                          that.sockets.to(name).emit('query', room.query())
+                        }
+                      }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
+                    } else {
+                      room.setStatus(that.config.room.STATUS_TERMINATED)
+                      that.sockets.to(name).emit('query', room.query())
+                    }
+                  }, (that.config.room.WAIT_TIME_VIDEO_CONVERSATION + that.config.room.NETWORK_RESPONSE_DELAY))
+                } else {
+                  room.setStatus(that.config.room.STATUS_TERMINATED)
+                  that.sockets.to(name).emit('query', room.query())
+                }
+              }, (that.config.room.WAIT_TIME_RESULT_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
             } else {
               room.setStatus(that.config.room.STATUS_TERMINATED)
               that.sockets.to(name).emit('query', room.query())
-            } }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-          } else {
-            room.setStatus(that.config.room.STATUS_TERMINATED)
-            that.sockets.to(name).emit('query', room.query())
-          } }, (that.config.room.WAIT_TIME_VIDEO_CONVERSATION + that.config.room.NETWORK_RESPONSE_DELAY))
+            }
+          }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
         } else {
           room.setStatus(that.config.room.STATUS_TERMINATED)
           that.sockets.to(name).emit('query', room.query())
-        } }, (that.config.room.WAIT_TIME_RESULT_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-      } else {
-        room.setStatus(that.config.room.STATUS_TERMINATED)
-        that.sockets.to(name).emit('query', room.query())
-      } }, (that.config.room.WAIT_TIME_SELECTION_SCREEN + that.config.room.NETWORK_RESPONSE_DELAY))
-    } else {
-        room.setStatus(that.config.room.STATUS_TERMINATED)
-        that.sockets.to(name).emit('query', room.query())
-    } }, (this.config.room.WAIT_TIME_AUDIO_CONVERSATION + this.config.room.NETWORK_RESPONSE_DELAY))
+        }
+      }, (this.config.room.WAIT_TIME_AUDIO_CONVERSATION + this.config.room.NETWORK_RESPONSE_DELAY))
+    } catch (e) {
+      this.logger.error('[TIMER] Error', e)
+    }
   }
 
   bindSocketToPublicEvents(socket) {
