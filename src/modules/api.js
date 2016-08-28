62e669786c7f005f93831339f897a1b8bec9063f
Nyan Of The Moon <nyanofthemoon@gmail.com>
2016-08-28 07:01:53 -0400

    d

--- src/modules/api.js
+++ src/modules/api.js
@@ -453,6 +453,8 @@ class Api {
     try {
       let user = this.getUserBySocketId(socket.id)
       if (user) {
+
+        console.log('1')
         this.leave(socket)
         let roomName = null
         let room     = null
@@ -460,10 +462,15 @@ class Api {
 
           default:
           case 'match':
+
+            console.log('2')
+
             let ageGroup = user.getAgeRange()
             let roomType = data.type
             let name     = data.kind + '_' + roomType + '_' + socket.id + '/' + Math.floor((Math.random() * 999999))
             let genderMatch;
+
+            console.log('3')
 
             // @NOTE TO DO
             // In the final screen you can either confirm, reject or report(thats a reject)
@@ -480,10 +487,7 @@ class Api {
               genderMatch = user.getWantedGenderFriend()
             }
 
-            //@TODO Remove me. This is for testing with just 2 devices//
-            genderMatch   = 'AA'
-            ageGroup      = '18-29'
-            ////////////////////////////////////////////////////////////
+            console.log('4')
 
             for (let i = 0; i < parseInt(this.config.room.FIND_BY_QUERY_RETRIES); i++) {
               let tempRoom = this.getNextRoomByQuery(genderMatch, ageGroup, roomType)
@@ -500,9 +504,14 @@ class Api {
               }
             }
 
+            console.log('5')
+
             roomName = name
             let joined = true
             if (!room) {
+
+              console.log('6A')
+
               room = new Room(this.config)
               room.setInitiator(user.getId())
               room.initialize(this.sockets, {
@@ -515,23 +524,40 @@ class Api {
               })
               joined = false
             } else {
+
+              console.log('6B')
+
               roomName = room.getName()
             }
 
             socket.join(roomName)
+
+            console.log('7')
+
             socket.room = roomName
             if (joined) {
               this.removeRoomFromQueue(room)
+
+              console.log('8A')
+
               this.logger.info('[JOIN] Joined Room ' + roomName + ' having ' + roomType + ' ' + genderMatch + '/' + ageGroup)
               callback(room.getSocketIds())
+
+              console.log('8B')
+
               this.runTimer(room)
+              console.log('8C')
             } else {
               this.logger.info('[JOIN] Created Room ' + roomName + ' having ' + roomType + ' ' + genderMatch + '/' + ageGroup)
+
+              console.log('8A')
               socket.emit('query', room.query())
+              console.log('8B')
               // Reported Users Have To Wait WAIT_TIME_PER_USER_REPORT millis per reports before room queryable
               let that = this
               setTimeout(function() {
                 that.addRoom(room)
+                console.log('8C')
               }, (user.getReports() * parseInt(that.config.user.WAIT_TIME_PER_USER_REPORT)))
             }
             break;
